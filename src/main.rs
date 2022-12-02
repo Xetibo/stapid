8d85feb1816b82b78613149897701b402e34d18b
Fabio Lenherr <fabio.lenherr@gmail.com>
2022-12-02 09:39:47 +0100

    feat: add working life system

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
-use bevy::{
-    prelude::*,
-    sprite::collide_aabb::{collide, Collision},
-    sprite::MaterialMesh2dBundle,
-};
-use bevy_inspector_egui::{Inspectable, WorldInspectorPlugin};
+use bevy::{prelude::*, sprite::collide_aabb::collide, sprite::MaterialMesh2dBundle};
+use bevy_inspector_egui::{Inspectable, RegisterInspectable, WorldInspectorPlugin};
+use std::time::Duration;
 
 #[derive(Component, Inspectable)]
 pub struct Player {
     pub lifes: i32,
+    pub invulnerable: bool,
     pub speed: f32,
     pub direction: Direction,
+    pub name: String,
 }
 
 #[derive(Component, Inspectable)]
@@ -32,12 +31,20 @@ struct Name(String);
 #[derive(Component)]
 struct Collider;
 
+#[derive(Component)]
+struct HitCooldownTimer {
+    timer: Timer,
+    associated_player: String,
+}
+
 impl Player {
-    fn new() -> Player {
+    fn new(entered_name: String) -> Player {
         Player {
             lifes: 3,
+            invulnerable: false,
             speed: 2.5,
             direction: Direction::Up,
+            name: entered_name,
         }
     }
 
@@ -67,20 +74,37 @@ fn main() {
             ..default()
         }))
         .add_plugin(WorldInspectorPlugin::new())
+        .register_inspectable::<Player>()
+        .register_inspectable::<Bullet>()
         .add_startup_system(spawn_player)
         .add_startup_system(spawn_camera)
         .add_system(move_all_players)
         .add_system(player_shoot)
         .add_system(move_all_bullets)
         .add_system(collision_bullet)
+        .add_system(tick_timer)
         .run();
 }
 
 fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
-        Player::new(),
+        Player::new(String::from("player1")),
         SpriteBundle {
+            sprite: Sprite {
+                custom_size: Option::Some(Vec2 { x: 50.0, y: 50.0 }),
+                ..default()
+            },
             texture: asset_server.load("../assets/stick.resized.png"),
+            transform: Transform::from_translation(Vec3 {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0,
+            })
+            .with_scale(Vec3 {
+                x: 1.0,
+                y: 1.0,
+                z: 0.0,
+            }),
             ..default()
         },
         Collider,
@@ -88,9 +112,23 @@ fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
         Name(String::from("player1")),
     ));
     commands.spawn((
-        Player::new(),
+        Player::new(String::from("player2")),
         SpriteBundle {
+            sprite: Sprite {
+                custom_size: Option::Some(Vec2 { x: 50.0, y: 50.0 }),
+                ..default()
+            },
             texture: asset_server.load("../assets/stick.resized.png"),
+            transform: Transform::from_translation(Vec3 {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0,
+            })
+            .with_scale(Vec3 {
+                x: 1.0,
+                y: 1.0,
+                z: 0.0,
+            }),
             ..default()
         },
         Collider,
@@ -136,27 +174,52 @@ fn move_all_bullets(
 
 fn collision_bullet(
     mut commands: Commands,
-    mut bullet_query: Query<&Transform, With<Bullet>>,
+    bullet_query: Query<&Transform, With<Bullet>>,
     mut collider_query: Query<(Entity, &Transform, Option<&mut Player>), With<Collider>>,
 ) {
-    for bullet_transform in &mut bullet_query {
-        let bullet_size = bullet_transform.scale.truncate();
+    for bullet_transform in &bullet_query {
+        let bullet_size = Vec2 { x: 10.0, y: 10.0 };
+        let player_size = Vec2 { x: 50.0, y: 50.0 };
         for (collider_entity, transform, mut maybe_player) in &mut collider_query {
             let collision = collide(
                 bullet_transform.translation,
                 bullet_size,
                 transform.translation,
-                transform.scale.truncate(),
+                player_size,
             );
             if collision.is_some() {
                 if maybe_player.is_some() {
                     let player = &mut **maybe_player.as_mut().unwrap();
-                    if player.lifes > 0 {
-                        player.decrement_life();
-                    } else {
-                    commands.entity(collider_entity).despawn();
+                    if player.invulnerable == false {
+                        if player.lifes > 1 {
+                            player.decrement_life();
+                            player.invulnerable = true;
+                            commands.spawn((HitCooldownTimer {
+                                timer: Timer::new(Duration::from_secs(2), TimerMode::Once),
+                                associated_player: player.name.clone(),
+                            },));
+                        } else {
+                            commands.entity(collider_entity).despawn();
+                        }
                     }
                 }
+            }
+        }
+    }
+}
+
+fn tick_timer(
+    mut commands: Commands,
+    mut player_query: Query<&mut Player>,
+    mut timer_query: Query<(Entity, &mut HitCooldownTimer)>,
+    time: Res<Time>,
+) {
+    for (entity, mut hit_timer) in &mut timer_query {
+        hit_timer.timer.tick(time.delta());
+        for mut player in &mut player_query {
+            if hit_timer.timer.finished() && hit_timer.associated_player == player.name {
+                player.invulnerable = false;
+                commands.entity(entity).despawn();
             }
         }
     }
@@ -217,16 +280,84 @@ fn player_shoot(
     for (player, name, transform) in &players {
         if name.0 == "player1" {
             if keys.just_pressed(KeyCode::LControl) {
-                commands.spawn((
-                    Bullet::new(player.direction.clone()),
-                    MaterialMesh2dBundle {
-                        mesh: meshes.add(shape::Circle::new(10.0).into()).into(),
-                        material: materials.add(Color::rgb(1.0, 0.0, 0.0).into()),
-                        transform: *transform,
-                        ..default()
-                    },
-                    Name(String::from("Bullet from player1")),
-                ));
+                match player.direction {
+                    Direction::Up => {
+                        commands.spawn((
+                            Bullet::new(player.direction.clone()),
+                            MaterialMesh2dBundle {
+                                mesh: meshes.add(shape::Circle::new(10.0).into()).into(),
+                                material: materials.add(Color::rgb(1.0, 0.0, 0.0).into()),
+                                transform: Transform::from_translation(
+                                    transform.translation
+                                        + Vec3 {
+                                            x: 0.0,
+                                            y: 50.0,
+                                            z: 0.0,
+                                        },
+                                ),
+                                ..default()
+                            },
+                            Name(String::from("Bullet from player1")),
+                        ));
+                    }
+                    Direction::Down => {
+                        commands.spawn((
+                            Bullet::new(player.direction.clone()),
+                            MaterialMesh2dBundle {
+                                mesh: meshes.add(shape::Circle::new(10.0).into()).into(),
+                                material: materials.add(Color::rgb(1.0, 0.0, 0.0).into()),
+                                transform: Transform::from_translation(
+                                    transform.translation
+                                        + Vec3 {
+                                            x: 0.0,
+                                            y: -50.0,
+                                            z: 0.0,
+                                        },
+                                ),
+                                ..default()
+                            },
+                            Name(String::from("Bullet from player1")),
+                        ));
+                    }
+                    Direction::Right => {
+                        commands.spawn((
+                            Bullet::new(player.direction.clone()),
+                            MaterialMesh2dBundle {
+                                mesh: meshes.add(shape::Circle::new(10.0).into()).into(),
+                                material: materials.add(Color::rgb(1.0, 0.0, 0.0).into()),
+                                transform: Transform::from_translation(
+                                    transform.translation
+                                        + Vec3 {
+                                            x: 50.0,
+                                            y: 0.0,
+                                            z: 0.0,
+                                        },
+                                ),
+                                ..default()
+                            },
+                            Name(String::from("Bullet from player1")),
+                        ));
+                    }
+                    Direction::Left => {
+                        commands.spawn((
+                            Bullet::new(player.direction.clone()),
+                            MaterialMesh2dBundle {
+                                mesh: meshes.add(shape::Circle::new(10.0).into()).into(),
+                                material: materials.add(Color::rgb(1.0, 0.0, 0.0).into()),
+                                transform: Transform::from_translation(
+                                    transform.translation
+                                        + Vec3 {
+                                            x: -50.0,
+                                            y: 0.0,
+                                            z: 0.0,
+                                        },
+                                ),
+                                ..default()
+                            },
+                            Name(String::from("Bullet from player1")),
+                        ));
+                    }
+                }
             }
         }
     }
